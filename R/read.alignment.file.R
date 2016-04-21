8917c8e48a684a1b687e32a10b9fa5a694346edd
P Hraber <peterhraber@gmail.com>
2016-04-21 10:35:13 -0600

    Verify the aligned sequences are all the same length.

--- R/read.alignment.file.R
+++ R/read.alignment.file.R
@@ -22,10 +22,53 @@ read.alignment.file <- function(aas_file=NULL, alignment_format="fasta") {
     try(a <- seqinr::read.alignment(aas_file, alignment_format, 
         forceToLower=F))
 
+    if (!is.null(a)) {
+
+        if (length(table(nchar(a$seq))) > 1) {
+
+	    a.table <- sort(table(nchar(a$seq)), decreasing=T)
+
+	    length.summary <- paste0("There are ", names(a.table)[1], 
+		" sequences are of length ", a.table[1], 
+		".  Please review these ", sum(a.table[-1]), 
+		" sequences with different lengths: ")
+
+	    if (sum(a.table[-1]) == 1) {
+		length.summary <- paste0("There are ", names(a.table)[1], 
+		    " sequences are of length ", a.table[1], 
+		    ".  Please review this sequence with length ", 
+		    names(a.table)[2], ": ")
+	    } else if (length(a.table) == 2) {
+		length.summary <- paste0("There are ", names(a.table)[1], 
+		    " sequences are of length ", a.table[1], 
+		    ".  Please review these sequences with length ", 
+		    names(a.table)[2], ": ")
+            }
+
+	    other.seqnames <- paste(a$nam[which(nchar(a$seq) != 
+		    names(a.table)[1])], collapse=",")
+
+            stop(paste0("ERROR in read.alignment.file(): alignment file '", 
+		    aas_file, "' contains sequences of different lengths.  ",
+		    length.summary, other.seqnames))
+	}
+    }
+
     b = NULL
     if (!is.null(a))
         try(b <- seqinr::as.matrix.alignment(a))
 
+    if (!is.null(b)) {
+
+        if (is.list(b))
+            stop(paste0("ERROR in read.alignment.file(): alignment file '", 
+                aas_file, "' could not be converted to a character matrix."))
+
+        if (any(!is.character(c(b))))
+            stop(paste0("ERROR in read.alignment.file(): alignment file '", 
+                aas_file, 
+		"' contains elements that could not be treated as letters."))
+    }
     return ( b )
     # need to respond in case of error / failure above?
 }
